0ddfbfb2b675461f6ccd8fe8a8b32f0d767dcfd1
Emily Herrera <e.herrera@grupoub.com>
2024-11-11 14:17:48 +0100

    este es mi primer comentario

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Reserva de Coches</h1>
+
+    </div>
+  );
+};
+
+export default App;
